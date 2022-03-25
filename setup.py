bd2dd6d0afdc20b4a49001741dde5ec9698f66ce
Oscar Neira Garcia <oneirag@yahoo.es>
2022-03-25 05:45:46 +0100

    Promote to version 0.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='commodity_data',
-    version='0.2.2',
+    version='0.2.3',
     packages=['commodity_data', 'commodity_data.downloaders', 'commodity_data.downloaders.omip',
               'commodity_data.downloaders.barchart'],
     url='https://github.com/Oneirag/commodity_data.git',
